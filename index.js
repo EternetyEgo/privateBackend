99a79f1e91287b287e6a2e0ca65e7cb56ad763e1
EternityEgo <your-email@example.com>
2025-01-30 13:20:48 +0500

    ego

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const app = express();
 const port = process.env.PORT || 5050;
 
 require("./server/db")(app);
-require('./server/apis')(app)
+require("./server/apis")(app);
 
 app.listen(port, () => {
-  console.log(`Server running ${port}`);
+  console.log(`\u001b[33m 🚀 Server started. \n\u001b[32m Local IP: http://127.0.0.1:${port}`);
 });
